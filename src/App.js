7bacc381de794965e2fd54eb31b54bcb0ef76de0
Lara <laarasantos12@gmail.com>
2020-03-28 11:02:46 -0300

    Implementado class PostPefil

--- src/App.js
+++ src/App.js
@@ -1,63 +1,6 @@
 import React from "react";
 import "./style.css"
 
-// COLOQUE ESSE CÓDIGO NO SEU COMPONENTE DE CLASSE
-// state = {
-//   [
-//     {
-//       nome: 'Seu nome',
-//       ocupacao: 'Developer',
-//       resumo: '20 anos, solteira e vida loca.',
-//       perfilProfissional: 'Faço miojo bom',
-//       contatos: [
-//         {
-//           id: 1,
-//           tipo: 'telefone',
-//           contato: '19 0000-0000'
-//         },
-//         {
-//           id: 2,
-//           tipo: 'email',
-//           contato: 'nome@dev.com'
-//         }
-//       ],
-//       educacao: [
-//         {
-//           id: 1,
-//           instituicao: 'EMEF',
-//           curso: 'Ensino basico'
-//         },
-//         {
-//           id: 2,
-//           instituicao: 'ETEC',
-//           curso: 'Como ser dev'
-//         }
-//       ],
-//       experiencia: [
-//         {
-//           id: 1,
-//           cargo: 'Front',
-//           periodo: 'Agosto 2019',
-//           empresa: 'NASA',
-//           local: 'Nova York',
-//           conteudo: 'Fazia nada e ganhava bem',
-//           atual: true
-//         },
-//         {
-//           id: 2,
-//           cargo: 'Back',
-//           periodo: 'Agosto 2018 - Agosto 2019',
-//           empresa: 'CIA',
-//           local: 'Boston',
-//           conteudo: 'Fazia nada e ganhava mal',
-//           atual: false
-//         }
-//       ]
-//     }
-//   ]
-// }
-
-// quebre esse código em outros componentes e importe os componentes aqui
 function App() {
   return (
     <main>
---
+++ src/components/PostPerfil/index.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+
+class PostPerfil extends Component {
+
+    state = {
+   posts: [
+        {
+        nome: 'Seu nome',
+        ocupacao: 'Developer',
+        resumo: '20 anos, solteira e vida loca.',
+        perfilProfissional: 'Faço miojo bom',
+        contatos: [
+            {
+            id: 1,
+            tipo: 'telefone',
+            contato: '19 0000-0000'
+            },
+            {
+            id: 2,
+            tipo: 'email',
+            contato: 'nome@dev.com'
+            }
+        ],
+        educacao: [
+            {
+            id: 1,
+            instituicao: 'EMEF',
+            curso: 'Ensino basico'
+            },
+            {
+            id: 2,
+            instituicao: 'ETEC',
+            curso: 'Como ser dev'
+            }
+        ],
+        experiencia: [
+            {
+            id: 1,
+            cargo: 'Front',
+            periodo: 'Agosto 2019',
+            empresa: 'NASA',
+            local: 'Nova York',
+            conteudo: 'Fazia nada e ganhava bem',
+            atual: true
+            },
+            {
+            id: 2,
+            cargo: 'Back',
+            periodo: 'Agosto 2018 - Agosto 2019',
+            empresa: 'CIA',
+            local: 'Boston',
+            conteudo: 'Fazia nada e ganhava mal',
+            atual: false
+            }
+        ]
+        }
+    ]
+    }
+
+    render(){
+        const { posts } = this.state
+
+        return(
+            <div className = "post-perfil">{
+                posts.map(post => {
+                    <Post key={post.id} conteudo={post} />
+                })
+            }
+            </div>
+        )
+    }
+}
+
+export default PostPerfil
